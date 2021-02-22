11cc7b33a5a5e48e3d3bb87d72a270974ecfea53
santanatony <70248180+santanatony@users.noreply.github.com>
2021-02-21 16:25:32 -0800

    Add files via upload

---
+++ Pong2D/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+    public Paddle paddle;
+
+    public int p1Score;
+    public int p2Score;
+    public Text scoreText;
+    public int scoreToWin;
+    public Text winner;
+    public GameObject p1Win;
+    public GameObject p2Win;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+
+    void Start()
+    {
+        // Convert screen's pixel coordinate into game's position
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2 (0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2 (Screen.width, Screen.height));
+
+        // Create ball
+        Instantiate (ball);
+
+        // Create two paddles
+        Paddle paddle1 = Instantiate (paddle) as Paddle;
+        Paddle paddle2 = Instantiate (paddle) as Paddle;
+        paddle1.Init (true); // right paddle
+        paddle2.Init (false); // left paddle
+
+    }
+
+    public void Score(bool p1)
+    {
+        if (p1)
+        {
+            p1Score++;
+            // Player 1 wins
+            if (p1Score >= scoreToWin)
+            {
+                p1Win.gameObject.SetActive(true);
+                Debug.Log("Player 1 wins!");
+                Time.timeScale = 0;
+                enabled = false;
+            }
+        }
+        else
+        {
+            p2Score++;
+            // Player 2 wins
+            if (p2Score >= scoreToWin)
+            {
+                p2Win.gameObject.SetActive(true);
+                Debug.Log("Player 2 wins!");
+                Time.timeScale = 0;
+                enabled = false;
+            }
+        }
+
+        scoreText.text = p1Score.ToString() + " : " + p2Score.ToString();
+    }
+
+    
+}
---
+++ Pong2D/Scripts/Paddle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    float height;
+
+    string input;
+    public bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+    }
+
+    public void Init(bool isRightPaddle) {
+
+        isRight = isRightPaddle;
+
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle) {
+            // Place paddle on right of screen
+            pos = new Vector2(GameManager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x; // Move a bit to the left
+
+            input = "PaddleRight";
+
+        } else {
+            // Place paddle on left of screen
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x; // Move a bit to the right
+
+            input = "PaddleLeft";
+        }
+
+        // update paddle's position
+        transform.position = pos;
+
+        transform.name = input;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        // Restrict Paddle movement
+        // Stops paddle from moving too far up/down
+        if (transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0)
+        {
+            move = 0;
+        }
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0)
+        {
+            move = 0;
+        }
+
+        transform.Translate(move * Vector2.up);
+    }
+}
