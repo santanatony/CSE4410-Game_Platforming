f5961cf23fa24800aaf9dd1b6333678482916702
santanatony <70248180+santanatony@users.noreply.github.com>
2021-05-16 10:03:58 -0700

    Add files via upload

---
+++ Escape Room/Scripts/ObjectsManage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsManage : MonoBehaviour
+{
+    private DisplayImage currentDisplay;
+
+    public GameObject[] ObjectsToManage;
+    public GameObject[] UIRenderObjects;
+
+    void Start()
+    {
+        currentDisplay = GameObject.Find("displayImage").GetComponent<DisplayImage>();
+        RenderUI();
+    }
+
+    void Update()
+    {
+        ManageObjects();
+    }
+
+    void ManageObjects()
+    {
+        for (int i = 0; i < ObjectsToManage.Length; i++)
+        {
+            if (ObjectsToManage[i].name == currentDisplay.GetComponent<SpriteRenderer>().sprite.name)
+            {
+                ObjectsToManage[i].SetActive(true);
+            }
+            else
+                ObjectsToManage[i].SetActive(false);
+        }
+    }
+
+    void RenderUI()
+    {
+        for (int i = 0; i < UIRenderObjects.Length; i++)
+        {
+            UIRenderObjects[i].SetActive(false);
+        }
+    }
+}
